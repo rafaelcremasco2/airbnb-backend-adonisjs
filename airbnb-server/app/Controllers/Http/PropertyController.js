5a594e1a488400dc8e0e36db3bf97656dc6996c4
Rafael Cremasco Lacerda <rafaelcremasco@hotmail.com>
2021-11-06 10:33:42 -0300

    Upload de imagens e geolocalização no AdonisJS

--- airbnb-server/app/Controllers/Http/PropertyController.js
+++ airbnb-server/app/Controllers/Http/PropertyController.js
@@ -14,13 +14,30 @@ class PropertyController {
    * Show a list of all properties.
    * GET properties
    */
-  async index() {
-    const properties = Property.all()
+  async index({ request }) {
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query()
+      .with('images')
+      .nearBy(latitude, longitude, 10)
+      .fetch()
 
     return properties
   }
 
-  async store({ request, response }) {
+  async store({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
   }
 
   /**
@@ -42,6 +59,21 @@ class PropertyController {
    * @param {Response} ctx.response
    */
   async update({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    property.merge(data)
+
+    await property.save()
+
+    return property
   }
 
   /**
